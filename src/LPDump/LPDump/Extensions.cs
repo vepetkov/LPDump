304bb3feb2c62fc317352b217592b619b6ef3706
V. Petkov <ve.petkov@gmail.com>
2014-05-14 14:52:10 +0200

    Support for a description line and expansion level
    Extended the dump function to support different expansion levels and, optionally, add a description line when generating the html dump.

--- src/LPDump/LPDump/Extensions.cs
+++ src/LPDump/LPDump/Extensions.cs
@@ -5,11 +5,12 @@ namespace LPDump
 {
     public static class Extensions
     {
-        public static T Dump<T>(this T source)
+        public static T Dump<T>(this T source, string description = "", int level = 5)
         {
             var localUrl = Path.GetTempFileName() + ".html";
-            using (var writer = LINQPad.Util.CreateXhtmlWriter(true))
+            using (var writer = LINQPad.Util.CreateXhtmlWriter(true, level, false))
             {
+                writer.WriteLine(LINQPad.Util.RawHtml(string.Format("<div style=\"font-weight:bold;background-color: white;color: green;\">{0}</div>", description)));
                 writer.Write(source);
                 File.WriteAllText(localUrl, writer.ToString());
             }
